f4f622f1d9f33231bb43bc36c0b785a90b8b2200
mbuguajnr23 <mbuguaian32@gmail.com>
2024-07-20 10:28:43 +0300

    modified server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const upload = multer();
 const productRoute = require('./routes/api/productRoute');
 
 // Connecting to the Database
-let mongodb_url = 'mongodb://localhost/';
+let mongodb_url = 'mongodb://127.0.0.1';
 let dbName = 'yolomy';
 
 // define a url to connect to the database
@@ -46,4 +46,3 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, ()=>{
     console.log(`Server listening on port ${PORT}`)
 })
-
